8ef15783b3352d43486a7dd8abd62f9bf7777d9f
Eric Almeida <37628478+almeieric@users.noreply.github.com>
2018-09-04 20:41:11 -0300

    Add files via upload

---
+++ aula/aula/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula
+{
+    class Pessoa
+    {
+        //atributos
+        private string nome;
+        private int idade;
+        private double altura;
+        private double peso;
+        private string cpf;
+
+        //metodo construtor
+        public Pessoa()
+        {
+        
+        
+            this.nome = "";
+            this.idade = 0;
+            this.altura = 0.0;
+            this.peso = 0.0;
+            this.cpf = "";
+        }
+        public Pessoa(string cpf, string nome)
+        {
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+        public string NomeCPF()
+        {
+            return this.nome + " - " + this.cpf;
+        }
+
+    }//fim da pessoa
+
+}//fim do aula
---
+++ aula/aula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa2 = new Pessoa("12675877689 ", "Eric Almeida");
+            Console.WriteLine(pessoa2.NomeCPF());
+            Console.WriteLine("\n\n\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+
+}
